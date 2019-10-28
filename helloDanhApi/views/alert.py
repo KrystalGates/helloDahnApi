cb0e51acb5ebc30ccf46a09a9c1470e63f8c4e56
krystal gates <krystal_sabrina@yahoo.com>
2019-10-28 09:44:18 -0500

    get user, alert and contact

--- helloDanhApi/views/alert.py
+++ helloDanhApi/views/alert.py
@@ -19,7 +19,7 @@ class AlertSerializer(serializers.HyperlinkedModelSerializer):
             view_name='alert',
             lookup_field='id'
         )
-        fields = ('id', 'url', 'alert_enabled', 'alert', 'alert_placement')
+        fields = ('id', 'url', 'alert_enabled', 'alert', 'alert_placement_id')
         depth = 1
 
 
